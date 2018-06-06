6b140fdd8ae249c93f5a84ef69cc01cea110df7f
dima <pochka2006@gmail.com>
2018-06-06 17:41:23 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Text from './components/Text'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Text title='Welcome to My Second React App'/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Text.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const Text = props => (
+    <div>
+        <h2>{props.title}</h2>
+        <h3>{props.text}</h3>
+    </div>
+);
+export default Text

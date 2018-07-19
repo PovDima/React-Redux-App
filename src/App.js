b6f241a253acdd495fb35a5c67df3c3fba901be0
dima <pochka2006@gmail.com>
2018-07-19 14:13:36 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Text from './components/Text'
-
+import Footer from './components/Footer'
+import AddTodo from './containers/AddTodo'
+import VisibleTodoList from './containers/VisibleTodoList'
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Text title='Welcome to My Second React App'/>
+        <Text title='Welcome to My Second React App' />
+        <Text text='Todo App'/>
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import rootReducer from './reducers'
+const store = createStore(rootReducer);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
